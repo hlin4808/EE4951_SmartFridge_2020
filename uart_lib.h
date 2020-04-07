acaa83bb557a93fdbfd470423db196dc3f0408c6
linx0405 <linx0405@umn.edu>
2020-04-07 17:15:16 -0500

    uart_lib.h from 3/21/2020
    Working library for controlling motor to step 1/50 full revolution. Working library for sending and receiving data with app. main.c has example for sending "hello world" and "abcd...123..." and example of moving motor one full revolution

--- uart_lib.h
+++ uart_lib.h
@@ -5,7 +5,18 @@
 extern "C" {
 #endif
 
+    #define RX_SIZE 50
+    #define TX_SIZE 50
+    
+    //Convenient macrso to do IOUNLOCK
+    #define PPSUnLock() {SYSKEY=0x0;SYSKEY=0xAA996655;SYSKEY=0x556699AA;CFGCONbits.IOLOCK=0;} 
+    #define PPSLock() {SYSKEY=0x0;SYSKEY=0xAA996655;SYSKEY=0x556699AA;CFGCONbits.IOLOCK=1;}
+    
     void uart_init();
+    void tx_data(const char data[]);
+    void SendChar(char c);
+    void SendString(char *string);
+    char ReadChar(void);
 
 #ifdef	__cplusplus
 }
