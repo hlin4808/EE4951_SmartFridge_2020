eaecd179b2157d15d278887798971b91f32fe817
linx0405 <linx0405@umn.edu>
2020-04-07 16:47:55 -0500

    main.c from 2/25/2020
    Device configuration with #pragma

---
+++ main.c
@@ -0,0 +1,90 @@
+/* 
+ * File:   main_v01.c
+ * Author: exehl
+ *
+ * Created on February 20, 2020, 10:59 AM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <xc.h>
+#include <cp0defs.h>
+#include <sys/attribs.h>
+
+/*** DEVCFG0 ***/
+
+#pragma config DEBUG =      OFF
+#pragma config JTAGEN =     OFF
+#pragma config ICESEL =     ICS_PGx1            // use PGEC1 and PGED1
+#pragma config TRCEN =      OFF
+#pragma config BOOTISA =    MIPS32
+#pragma config FSLEEP =     OFF
+#pragma config DBGPER =     PG_ALL
+#pragma config SMCLR =      MCLR_NORM
+#pragma config SOSCGAIN =   GAIN_2X
+#pragma config SOSCBOOST =  ON
+#pragma config POSCGAIN =   GAIN_LEVEL_3
+#pragma config POSCBOOST =  ON
+#pragma config EJTAGBEN =   NORMAL
+#pragma config CP =         OFF
+
+/*** DEVCFG1 ***/
+
+#pragma config FNOSC =      SPLL            // choose PLL for SYSCLK
+#pragma config DMTINTV =    WIN_127_128
+#pragma config FSOSCEN =    ON
+#pragma config IESO =       ON
+#pragma config POSCMOD =    EC
+#pragma config OSCIOFNC =   OFF
+#pragma config FCKSM =      CSECME
+#pragma config WDTPS =      PS1048576
+#pragma config WDTSPGM =    STOP
+#pragma config FWDTEN =     OFF                 // watchdog off
+#pragma config WINDIS =     NORMAL
+#pragma config FWDTWINSZ =  WINSZ_25
+#pragma config DMTCNT =     DMT31
+#pragma config FDMTEN =     OFF
+/*** DEVCFG2 ***/
+
+#pragma config FPLLRNG =    RANGE_8_16_MHZ
+#pragma config FPLLICLK =   PLL_FRC         // PLL source is 8MHz FRC
+#pragma config FPLLIDIV =   DIV_1           // PLL divide by 1
+#pragma config FPLLMULT =   MUL_16          // PLL multiply by 16
+#pragma config FPLLODIV =   DIV_2           // PLL divide by 2
+                                            // PLL - SYSCLK is 64MHz
+#pragma config VBATBOREN =  ON
+#pragma config DSBOREN =    ON
+#pragma config DSWDTPS =    DSPS32
+#pragma config DSWDTOSC =   LPRC
+#pragma config DSWDTEN =    OFF
+#pragma config FDSEN =      ON
+#pragma config BORSEL =     HIGH
+#pragma config UPLLEN =     OFF
+/*** DEVCFG3 ***/
+
+#pragma config USERID =     0xffff
+#pragma config FUSBIDIO2 =   ON
+#pragma config FVBUSIO2 =  ON
+#pragma config PGL1WAY =    ON
+#pragma config PMDL1WAY =   ON
+#pragma config IOL1WAY =    ON
+#pragma config FUSBIDIO1 =   ON
+#pragma config FVBUSIO1 =  ON
+#pragma config PWMLOCK =  OFF
+
+/*** BF1SEQ0 ***/
+
+#pragma config TSEQ =       0x0000
+#pragma config CSEQ =       0xffff
+
+int main(void) {
+
+    while(1)
+    {
+        
+    }
+    
+    
+    while(1);   // never reach here, but have forever loop just in case
+    return 0;
+}
