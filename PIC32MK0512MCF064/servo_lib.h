98bfb718244ed840a61b81b51c908fc6b6f9d7b8
hlin4808 <32402351+hlin4808@users.noreply.github.com>
2020-04-14 18:55:58 -0500

    initial commit for servo_lib.h
    Created init and set compare functions for Output Compare 2, OC2 is used to control the servo

---
+++ PIC32MK0512MCF064/servo_lib.h
@@ -0,0 +1,15 @@
+#ifndef _EXAMPLE_FILE_NAME_H    /* Guard against multiple inclusion */
+#define _EXAMPLE_FILE_NAME_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+    void servo_init(void);
+    void servo_setCompare(int val);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _EXAMPLE_FILE_NAME_H */
