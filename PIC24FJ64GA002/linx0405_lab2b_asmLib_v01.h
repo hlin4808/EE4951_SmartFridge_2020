46b82845aa02091fa5d24a9df6d4326c570ce200
hlin4808 <32402351+hlin4808@users.noreply.github.com>
2020-04-23 21:43:02 -0500

    Delays Library
    Delays library

---
+++ PIC24FJ64GA002/linx0405_lab2b_asmLib_v01.h
@@ -0,0 +1,25 @@
+/* 
+ * File:   linx0405_lab2b_asmLib_v01.h
+ * Author: exehl
+ *
+ * Created on September 12, 2019, 11:12 PM
+ */
+
+#ifndef LINX0405_LAB2B_ASMLIB_V01_H
+#define	LINX0405_LAB2B_ASMLIB_V01_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+    void wait_1us(void);
+    void wait_10us(void);
+    void wait_100us(void);
+    void wait_1ms(void);
+    void write_0(void);
+    void write_1(void);
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* LINX0405_LAB2B_ASMLIB_V01_H */
+
